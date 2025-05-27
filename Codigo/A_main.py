66cd1750566f3c5a492881764ad7e3986d025094
FelipeTamaki <pepetamaki@gmail.com>
2025-05-27 17:06:05 -0300

    pEPE

--- Codigo/A_main.py
+++ Codigo/A_main.py
@@ -5,6 +5,12 @@ from C_conexion import Conexion
 from D_leer_csv import *
 
 crear_nodos("nodos.csv")
+print("Impresion de nodos para comprobar que se cargaron bien:")
 Nodo.imprimir_nodos() # Prueba
+print("")
+print("Impresion de conexiones para comprobar que se cargaron bien:")
 crear_conexiones("conexiones.csv")
-Conexion.imprimir_conexiones() 
+Conexion.imprimir_conexiones() # Prueba
+print("")
+print("Impresion de los nodos de vuelta para ver si se hizo bien la conexion (No se si esta bien hecha la conexion del objeto pero de las redes funciona):")
+Nodo.imprimir_nodos()
