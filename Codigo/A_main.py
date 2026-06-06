2f0ff8e11ddcb23c5f60ae2e96e72d29615e2151
Felipe Tamaki <pepetamaki@gmail.com>
2026-06-06 13:54:29 -0300

    Arreglado

--- Codigo/A_main.py
+++ Codigo/A_main.py
@@ -1,10 +1,10 @@
-# Este archivo es para probar el codigo y que todo funcione bien, si ponen codigo en otros archivos para probar pongan el if __name__ == "__main__":
+from G_menu import *
+from E_datos_transportes import Fluvial, Aereo, Automotor, Ferroviario
+import matplotlib.pyplot as plt
+from H_planificador import *
 
-from B_nodo import Nodo
-from C_conexion import Conexion
-from D_leer_csv import *
 
-crear_nodos("nodos.csv")
-Nodo.imprimir_nodos() # Prueba
-crear_conexiones("conexiones.csv")
-Conexion.imprimir_conexiones() 
+if __name__ == "__main__":
+    m = tk.Tk()
+    app = Menu(m)
+    m.mainloop()
